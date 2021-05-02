992f24d6a6f5537216c6bec9bc5a9356dcc4c188
kuvera <kuvera22@mail.ru>
2021-05-02 20:28:33 +0300

    trash

--- src/main/java/com/example/domain/Lesson.java
+++ src/main/java/com/example/domain/Lesson.java
@@ -13,15 +13,68 @@ public class Lesson {
     @OneToOne
     private Teacher teacher;
     private StudentGroup studentGroup;
-
     @PlanningVariable(valueRangeProviderRefs = "timeslotRange")
     private Timeslot timeslot;
-
     @PlanningVariable(valueRangeProviderRefs = "roomRange")
     private Room room;
 
     private Lesson() {
     }
 
+    public Lesson(UUID id, String name, Teacher teacher, StudentGroup studentGroup, Timeslot timeslot, Room room) {
+        this.id = id;
+        this.name = name;
+        this.teacher = teacher;
+        this.studentGroup = studentGroup;
+        this.timeslot = timeslot;
+        this.room = room;
+    }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public StudentGroup getStudentGroup() {
+        return studentGroup;
+    }
+
+    public void setStudentGroup(StudentGroup studentGroup) {
+        this.studentGroup = studentGroup;
+    }
+
+    public Timeslot getTimeslot() {
+        return timeslot;
+    }
+
+    public void setTimeslot(Timeslot timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 }
--- src/main/java/com/example/domain/Teacher.java
+++ src/main/java/com/example/domain/Teacher.java
@@ -1,11 +1,80 @@
 package com.example.domain;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
+@TypeDefs({
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
 public class Teacher {
     @Id
     private UUID id;
+    private String firstName;
+    private String lastName;
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
+    private List<SubjectQuality> subjectQuality;
+    @ManyToOne
+    private Department department;
+
+    public Teacher() {
+    }
+
+    public Teacher(UUID id, String firstName, String lastName, List<SubjectQuality> subjectQuality, Department department) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.subjectQuality = subjectQuality;
+        this.department = department;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<SubjectQuality> getSubjectQuality() {
+        return subjectQuality;
+    }
+
+    public void setSubjectQuality(List<SubjectQuality> subjectQuality) {
+        this.subjectQuality = subjectQuality;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 }
