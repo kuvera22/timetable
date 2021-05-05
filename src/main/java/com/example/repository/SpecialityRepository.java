5eeda3f56be9d365722519c96984be7bbbeee5e1
daniil_mysnik_990 <69dantes6901>
2021-05-06 01:00:19 +0600

    front

--- src/main/java/com/example/repository/SpecialityRepository.java
+++ src/main/java/com/example/repository/SpecialityRepository.java
@@ -1,12 +1,15 @@
 package com.example.repository;
 
+import com.example.domain.Department;
 import com.example.domain.Speciality;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SpecialityRepository extends JpaRepository<Speciality, UUID> {
-
+    List<Speciality> getAllByDepartment(Department department);
 }
